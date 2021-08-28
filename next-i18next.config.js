616928301d5334d17d7937406bdb2c17cdff373e
Van Nguyen <thegoleffect@gmail.com>
2021-08-28 10:13:16 -0700

    restore to original config that was working

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,15 +1,15 @@
-const path = require("path");
+// const path = require("path");
 
-const defaultLocale = "en-US";
-const locales = ["en-US"];
+// const defaultLocale = "en-US";
+// const locales = ["en-US"];
 
-const localePath = path.resolve("./public/locales");
+// const localePath = path.resolve("./public/locales");
 
-module.exports = {
-	i18n: {
-		locales,
-		defaultLocale,
-	},
-	ns: ["common"],
-	localePath,
-};
+// module.exports = {
+// 	i18n: {
+// 		locales,
+// 		defaultLocale,
+// 	},
+// 	ns: ["common"],
+// 	localePath,
+// };
